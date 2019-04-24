64ef8e5df9612c3f9b7193ba1b6ff144b8621af2
Anna Spysz <anna.spysz@stackery.io>
2019-04-24 11:50:44 -0700

    Function code

--- src/getDataFromTable/index.js
+++ src/getDataFromTable/index.js
@@ -1,6 +1,25 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
 
-  return {};
+exports.handler = async (event, context) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: process.env.TABLE_NAME
+  };
+
+  let items;
+  let entries = [];
+
+  try {
+    items = await dynamodb.scan(params).promise();
+    items.Items.forEach((item) => entries.push(item));
+    console.log(`Getting data from table ${process.env.TABLE_NAME} in the cloud!`);
+  } catch (error) {
+    console.log(error);
+  }
+
+  const output = entries.map(item => item.content).join("\n");
+  process.stdout.write(output);
+
+  return 'Ok!'
 };
